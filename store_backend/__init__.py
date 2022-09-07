11fde486608fa102ef1d90b947e5e35b543e3008
jck-bit <jckkinyanjui@gmail.com>
2022-09-07 19:30:26 +0300

    Added virtualenv

--- store_backend/__init__.py
+++ store_backend/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 
