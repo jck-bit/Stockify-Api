462179467ef04a88b0e7e3029011f12d8a372f2f
jck-bit <jckkinyanjui@gmail.com>
2022-09-12 15:14:53 +0300

    Added products and prducts to the database

---
+++ store_backend/products/m.py
@@ -0,0 +1,14 @@
+
+alls  = ['h', 't', 'h','h', 't', 'h','h', 't', 'h','h', 't', 'h']
+
+list_one= []
+
+list_two= []
+
+for all in alls:
+    if 't':
+        list_one.append()
+    else:
+        list_two.append()
+print(list_one)
+
