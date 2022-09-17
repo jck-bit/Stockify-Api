ff10a2618a2a672dfd33ee92e4fcdfde6e9171ae
jck-bit <jckkinyanjui@gmail.com>
2022-09-17 12:52:24 +0300

    Admin can add new account

--- store_backend/__init__.py
+++ store_backend/__init__.py
@@ -2,15 +2,20 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from flask_login import LoginManager
+from datetime import timedelta
+
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
 
 JWTManager(app)
 db = SQLAlchemy(app)
-LoginManager = LoginManager(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
 from store_backend.products.routes import products
--- store_backend/users/routes.py
+++ store_backend/users/routes.py
@@ -1,4 +1,3 @@
-from crypt import methods
 from flask import Blueprint,jsonify, request,make_response
 from store_backend import db, app
 from store_backend.models import Sales, User
@@ -11,22 +10,13 @@ from flask_jwt_extended import create_access_token
 
 users = Blueprint('users', __name__)
 
-@users.route('/users', methods=['POST'])
-def create_user():
-    data = request.get_json()
-    
-    hashed_password = generate_password_hash(data['password'], method='sha256')
-    new_user = User(public_id=str(uuid.uuid4()), username=data['username'], password=hashed_password, admin=False)
 
-    db.session.add(new_user)
-    db.session.commit()
-
-    return jsonify({"message": "New user created"})
 
 @users.route('/users', methods=['GET'])
+@login_required
 def get_all_users():
     users = User.query.all()
-    
+     
     output = []
 
     for user in users:
@@ -36,7 +26,8 @@ def get_all_users():
         user_data['public_id'] = user.public_id
         user_data['password'] = user.password
         user_data['sales'] = user.sales
-
+        user_data['admin'] = user.admin
+        
         output.append(user_data)
 
     return jsonify({"users": output})
@@ -56,10 +47,21 @@ def get_one_user(public_id):
     user_data['password'] = user.password
     user_data['image_file'] = user.image_file
     user_data['sales'] = user.sales
+    
 
     return jsonify({"user": user_data})
 
+@users.route('/users/<public_id>', methods=['PUT'])
+def promote_user(public_id):
 
+    user = User.query.filter_by(public_id=public_id).first()
+    if not user:
+        return jsonify({"message": "user not found"})
+
+    user.admin = True
+    db.session.commit()
+    
+    return jsonify({"message": "user has been promoted"})
 
 @users.route('/login', methods=["POST"])
 def create_token():
@@ -71,14 +73,51 @@ def create_token():
         msg = "The username field cannot be empty"
         return {"status": "Failed", "message": msg},401
     if password == "":
-         msg = "The password field cannot be empty"
-         return {"status": "Failed", "message": msg}, 401
+        msg = "The password field cannot be empty"
+        return {"status": "Failed", "message": msg}, 401
     
     user = User.query.filter_by(username=username).first()
 
     if not user or not check_password_hash(user.password, password):
         return {"status":"Failed","message": "Invalid username or password"}, 401
-    
+
+    login_user(user)
     access_token = create_access_token(identity=username)
     return  {"status":"success","token":access_token}, 200
-  
+
+
+@users.route('/users', methods=['POST'])
+def create_user():
+
+    # if not current_user.admin:
+    #     msg = "Cannot perform that function"
+    #     return {"status":"Failed", "message": msg},401
+    
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    confirm_password = request.json.get('confirm_password')
+
+    if username =="":
+         msg = "The username field cannot be empty"
+         return {"status": "Failed", "message": msg},401
+
+    if password == "":
+        msg = "The password field cannot be empty"
+        return {"status": "Failed", "message": msg}, 401
+    
+    if confirm_password != password:
+        msg = "The two passwords dont match"
+        return {"status": "Failed", "message": msg}, 401
+    
+    user = User.query.filter_by(username=username).first()
+
+    if user:
+        msg = "User alraedy exists.Try another username"
+        return {"status": "Failed", "message": msg}, 401
+    
+    
+    new_user = User(public_id=str(uuid.uuid4()), username=username, password=generate_password_hash(password), admin=False)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({"message": "New user created"})
