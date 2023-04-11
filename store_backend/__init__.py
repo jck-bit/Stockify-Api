ea719ac1f0fc547630a1d7df4544b0101d439893
jck-bit <jckkinyanjui@gmail.com>
2023-04-11 19:27:01 +0300

    removed migrate

--- store_backend/__init__.py
+++ store_backend/__init__.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
-from flask_migrate import Migrate
 from datetime import timedelta
 from config import Config
 
@@ -13,7 +12,6 @@ app.config.from_object(Config)
 
 db = SQLAlchemy()
 JWTManager(app)
-Migrate(app, db)
 db.init_app(app)
 CORS(app)
 
