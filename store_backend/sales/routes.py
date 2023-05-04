dfc66d44b62c624c10179c49bd2ef0b471b4cfa6
jck-bit <jckkinyanjui@gmail.com>
2023-05-04 21:44:47 +0300

    A product has now a default image

--- store_backend/sales/routes.py
+++ store_backend/sales/routes.py
@@ -21,3 +21,10 @@ def get_sale_by_id(id):
     sale = Sales.query.filter_by(id=id).first()
 
     return jsonify({'sale':[{'id':sale.id, 'user':sale.user_id, 'product_sold':sale.product_id,'date':sale.date_sold}]})
+
+@sales.route('/sales/user/<id>', methods=['GET'])
+@jwt_required()
+def get_sales_by_user(id):
+    sales = Sales.query.filter_by(user_id=id).all()
+    
+    return jsonify({'sales': [{'id': sale.id, 'total_sale':sale.total_sales, 'user': sale.user_id, 'date':sale.date_sold, 'product':sale.product_id} for sale in sales]})
