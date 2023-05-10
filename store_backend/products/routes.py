017650479ff7a278202decdfdf8787729c17ab50
jck-bit <jckkinyanjui@gmail.com>
2023-05-10 14:04:04 +0300

    user can now add image with or without image

--- store_backend/products/routes.py
+++ store_backend/products/routes.py
@@ -3,19 +3,53 @@ from store_backend import db
 from flask_login import login_required
 from store_backend.models import Product
 from flask_jwt_extended import  jwt_required
+from datetime import datetime
+from supabase  import create_client
+import os
+from werkzeug.utils import secure_filename
+
+
+url = os.getenv('SUPABASE_URL')
+key = os.getenv('SUPABASE_KEY')
+
+supabase =  create_client(url, key)
+
+
 
 products = Blueprint('products', __name__)
 
 @products.route('/products', methods=['POST'])
 @jwt_required()
 def post_product():
-    data = request.get_json()
 
-    new_product = Product(name=data['name'], price=data['price'], quantity=data['quantity'])
+    name = request.form.get('name', None)
+    price = request.form.get('price', None)
+    quantity = request.form.get('quantity', None)
+
+    image_file = request.files.get('product_pic', None)
+
+    #if no product pic, the product will get a default pic from the supabase bucket
+    if not image_file:
+        image_file = supabase.storage.from_("product_pics").get_public_url("product_default.jpg")
+    else:
+        filename = secure_filename(image_file.filename)
+        file_data = image_file.read()
+        bucket_name = 'product_pics'
+        supabase.storage.from_(bucket_name).upload(filename, file_data)
+
+        # remove the old image and create a new one
+        product = Product.query.filter_by(name=name).first()
+        if product and product.image_file != 'product_default.jpg':
+            supabase.storage.from_(bucket_name).remove(product.image_file)
+
+        image_url = supabase.storage.from_(bucket_name).get_public_url(filename)
+
+    new_product = Product(name=name, price=price, quantity=quantity, image_file=image_url)
     db.session.add(new_product)
     db.session.commit()
 
-    return jsonify({'message': 'New product Added!'})
+    return jsonify({'message': 'Product added successfully'})
+
 
 @products.route('/products', methods=['GET'])
 @jwt_required()
